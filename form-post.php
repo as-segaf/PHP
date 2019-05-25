c66ab1c6c7c7481ce5fdda0e7218468bda9dd548
as-segaf <ziyanassegaf20@gmail.com>
2019-05-25 13:26:56 +0700

    post

---
+++ form-post.php
@@ -0,0 +1,7 @@
+<form method="post" action="show-post.php">
+	<label>Nama</label>
+	<input type="text" name="nama">
+	<label>Umur</label>
+	<input type="number" name="umur">
+	<input type="submit" value="kirim">
+</form>
---
+++ show-post.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$nama = $_POST['nama'];
+	$umur = $_POST['umur'];
+
+	echo "nama saya adalah $nama";
+	echo "<br>";
+	echo "umur saya adalah $umur";
+
+ ?>
