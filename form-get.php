39a910a4dfcdb746e48d54c080e29d1b093dbe78
as-segaf <ziyanassegaf20@gmail.com>
2019-05-25 13:26:32 +0700

    get

---
+++ form-get.php
@@ -0,0 +1,7 @@
+<form method="get" action="show-get.php">
+	<label>Nama</label>
+	<input type="text" name="nama">
+	<label>Umur</label>
+	<input type="number" name="umur">
+	<input type="submit" value="kirim">
+</form>
---
+++ show-get.php
@@ -0,0 +1,10 @@
+<?php 
+	$nama = $_GET['nama'];
+	$umur = $_GET['umur'];
+
+	echo "nama saya adalah $nama";
+	echo "<br>";
+	echo "umur saya adalah $umur";
+
+
+ ?>
