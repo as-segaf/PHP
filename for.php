33b6ac545f5b09b2bd2672f96d33b5acffef870a
as-segaf <ziyanassegaf20@gmail.com>
2019-05-25 08:26:28 +0700

    for

---
+++ for.php
@@ -0,0 +1,13 @@
+<?php 
+	
+
+	//for di gunakan untuk perulangan yang sudah di tentukan berapa kali perulangannya
+	$kata = "testing";
+
+	//perulangan 10 kali
+	for ($i=0; $i <10; $i++) { 
+		echo "$kata $i <br>";
+	}
+
+
+ ?>
