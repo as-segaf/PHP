100a3b18d1a8914dd027895c313f0a6078d94691
as-segaf <ziyanassegaf20@gmail.com>
2019-05-25 07:40:11 +0700

    switch-case

---
+++ switch-case.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+	$a = 4;
+
+	switch ($a) {
+		case 1:
+			echo "data 1";
+			break;
+		case 2:
+			echo "data 2";
+			break;
+		case 3:
+			echo "data 3";# code...
+			break;
+		case 4:
+			echo "data 4";
+			break;
+		default:
+			echo "no data";
+			break;
+	}
+
+
+ ?>
