f0f14f0de3de113edba93ddbe07f88bb920ea39b
as-segaf <ziyanassegaf20@gmail.com>
2019-05-25 07:15:59 +0700

    tipe-data

---
+++ tipe-data.php
@@ -0,0 +1,21 @@
+<?php 
+
+	//tipe data string 
+	$a = "testing";
+
+	//tipe data integer
+	$a = 1;
+
+	//tipe data float
+	$a = 1.5;
+
+	//tipe data boolean
+	$a = true;
+	$b = false;
+
+	//tipe data array 
+	$a = array("data1", "data2", "data3");
+
+	
+
+ ?>
