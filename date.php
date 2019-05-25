5d4887e468c044523be21d6d3be48dec11ab76a4
as-segaf <ziyanassegaf20@gmail.com>
2019-05-25 12:51:03 +0700

    date

---
+++ date.php
@@ -0,0 +1,14 @@
+<?php 
+
+	echo date('l, d-m-Y');
+	echo "<br>";
+	echo date('d-M-Y');
+	echo "<br>";
+	echo date('D, d-M-Y');
+	echo "<br>";
+	echo date('h:i:s a');
+	echo "<br>";
+	echo date('l, d-M-Y h:i:s a');
+
+
+ ?>
