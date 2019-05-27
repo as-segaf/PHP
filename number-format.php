2c6f1f33998e67e1129a6906031e5acdf72da1cf
as-segaf <ziyanassegaf20@gmail.com>
2019-05-27 14:14:55 +0700

    number format

---
+++ number-format.php
@@ -0,0 +1,9 @@
+<?php 
+
+	//number_format(angka,angkadibelakangkoma,pemisahdesimal,pemisahribuan);
+	
+	$a = 12341040000000;
+	echo number_format($a,2). "<br>";
+	echo number_format($a,2,"-",".");
+
+ ?>
