ffd0fa235fdb23c3eac62d995582e738ae5c56eb
as-segaf <ziyanassegaf20@gmail.com>
2019-06-24 07:36:34 +0700

    intial commit

--- increment-decrement.php
+++ increment-decrement.php
@@ -22,5 +22,16 @@ echo "<h3>Predecrement</h3>";
 $a = 5;
 echo "\$a = $a <br>";
 echo "nilai \$a awal " .--$a. "<br>";
-echo "nilai \$a akhir $a";
- ?>
+echo "nilai \$a akhir $a <br>";
+
+function tes(){
+	global $b;
+	$b = 6;
+	global $a;
+	echo "$a <br>";
+}
+
+// global $b;
+echo "$b";
+
+?>
