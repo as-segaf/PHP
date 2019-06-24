4fa4341cfa6da719e12a1fac608d8da466d1832b
as-segaf <ziyanassegaf20@gmail.com>
2019-06-24 07:37:28 +0700

    sistem login

---
+++ tes/prosesRegister.php
@@ -0,0 +1,16 @@
+<?php 
+	include'../config.php';
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$role     = $_POST['role'];
+	$cek	  = mysqli_query($link, "SELECT * FROM user WHERE username='$username'");
+	$insert   = ("INSERT INTO user (username, password, role) VALUES ('$username', '$password', '$role')");
+
+	if (mysqli_num_rows($cek) > 0) {
+		echo "Username sudah terdaftar! <a href='register.php'>Kembali</a> ";
+
+	} else if (mysqli_query($link, $insert)) {
+		echo "Register berhasil, silahkan <a href='login.php'>login</a>";
+	}
+
+ ?>
